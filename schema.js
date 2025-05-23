514fb60159621fcee2b6617624ae180e9d08b23d
Aaditya Singh <aadityasingh9601@gmail.com>
2025-05-23 17:59:04 +0530

    Ready for deployment

--- schema.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const userSchema = Joi.object({
-  name: Joi.string().required(),
-  username: Joi.string().required().min(5).max(20),
-  password: Joi.string().required().min(8).max(16),
-});
-
-export default userSchema;
